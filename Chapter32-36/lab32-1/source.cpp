4ec85e2fa0bcb24f8e6ff303cd4a08a18f165642
m1suru-amornrat <684244110@mail.pbru.ac.th>
2025-08-28 21:12:43 +0700

    Add files via upload

---
+++ Chapter32-36/lab32-1/source.cpp
@@ -0,0 +1,15 @@
+#include "myheader.h" //include the header
+#include <iostream>
+int main()
+{
+myfunction();
+}
+// function definition
+void myfunction()
+{
+std::cout << "Hello World from multiple files.";
+}
+int mian(){
+  myfunction();
+  return 0;
+}
